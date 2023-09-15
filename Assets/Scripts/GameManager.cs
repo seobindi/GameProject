515e62998c16a2bed15ffa40d2a48aeaae654d5d
seobindi <seobiiiiii@gmail.com>
2023-09-15 17:36:24 +0900

    First

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
